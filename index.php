1b8418c3c60f488ebfc2d8e0e9c09456b0c29240
ridvanbaluyos <ridvan@baluyos.net>
2014-01-04 20:48:46 +0800

    Added Scrollback Chatbox.

--- index.php
+++ index.php
@@ -380,5 +380,20 @@
   ga('create', 'UA-45399063-2', 'ridvanbaluyos.com');
   ga('send', 'pageview');
 </script>
+
+<!-- Scrollback -->
+<script>
+  window.scrollback = {
+    streams:["geekneko"],
+    theme: 'light',
+    ticker: true,
+  };
+
+  /***** don't edit below *****/
+  (function(d,s,h,e){e=d.createElement(s);e.async=1;
+  e.src=h+'/client.min.js';scrollback.host=h;
+  d.getElementsByTagName(s)[0].parentNode.appendChild(e);}
+  (document,'script',location.protocol+'//scrollback.io'));
+</script>
 </body>
 </html>
